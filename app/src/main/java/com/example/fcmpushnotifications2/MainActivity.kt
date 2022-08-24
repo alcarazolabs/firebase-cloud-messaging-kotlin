4918d706eee94befcc7c2da13c7fc199b505ce44
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2022-08-24 17:44:11 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/fcmpushnotifications2/MainActivity.kt
+++ app/src/main/java/com/example/fcmpushnotifications2/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+          FirebaseMessaging.getInstance().token.addOnCompleteListener { task: Task<String> ->
+            if (!task.isSuccessful) {
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d("PUSH_TOKEN", "pushToken: $token")
+        }
+        
+        
     }
 }
